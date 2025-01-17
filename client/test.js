d7de1c6872952c4b964b04bc38a25a506f51433f
Platon223 <platon.tikhnenko@gmail.com>
2025-01-17 16:40:58 -0600

    Add files via upload

---
+++ client/test.js
@@ -0,0 +1,70 @@
+const socket = io('http://localhost:4000');
+
+document.querySelector('.dash').innerHTML = localStorage.getItem('dash-name');
+
+
+socket.on('test', data => {
+    console.log(data);
+})
+
+socket.on('launch-accounts', usernames => {
+    console.log(usernames);
+})
+
+
+socket.on('num-users', (users) => {
+    if(users === 1) {
+        document.querySelector('.users').innerHTML = `There is ${users} user online`;
+    } else {
+        document.querySelector('.users').innerHTML = `There are ${users} users online`;
+    }
+   
+});
+
+
+function check() {
+
+    const name = document.querySelector('.username').value;
+
+   
+    socket.on('launch-accounts', (usernames) => {
+        console.log(usernames);
+
+        if(name !== localStorage.getItem('dash-name')) {
+            const accName = localStorage.getItem('dash-name');
+            socket.emit('delete-acc', accName);
+        }
+
+        let nameInUse = false;
+        usernames.forEach(ur => {
+            if (ur.nm === name) {
+                nameInUse = true;
+            }
+        });
+
+    
+
+        if (nameInUse) {
+            console.log('This name is already in use.');
+        } else {
+            console.log('You are good to go.');
+
+            const loged = true;
+
+            const user = { nm: name, loged: loged };
+            socket.emit('send-acc', user);
+
+
+            const dashBoard = name;
+            localStorage.setItem('dash-name', dashBoard);
+
+            document.querySelector('.dash').innerHTML = localStorage.getItem('dash-name');
+
+        }
+
+    });
+  
+
+
+    socket.emit('request-accounts');
+}
